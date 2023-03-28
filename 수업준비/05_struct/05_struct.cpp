9aff9a9e7cfa927d436671dea3b1c2ac76c0f4be
sarah-spreatics <sarah@spreatics.com>
2023-03-28 12:25:59 +0900

    feat: prepare struct lesson

---
+++ 수업준비/05_struct/05_struct.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    struct Position {
+        int x = 0;
+        int y = 0;
+    };
+
+    Position p;
+
+    // p.x = 3;
+    // p.y = 5;
+
+    p = { 3, 4 };
+
+    cout << p.x << ' ' << p.y << std::endl;
+
+    // 실습 1
+
+    struct Rectangle {
+        double width, height;
+    };
+
+    Rectangle rectangle;
+
+    cout << "가로, 세로 길이를 입력하세요. ";
+    cin >> rectangle.width >> rectangle.height;
+
+    cout << "넓이는 : " << rectangle.width * rectangle.height;
+}
