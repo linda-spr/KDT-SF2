067a7172bbaaa857743c26bb7ef9bb91933ea185
sarah-spreatics <sarah@spreatics.com>
2023-04-07 11:57:43 +0900

    fix: c++ project 6

--- 수업준비/cplusplus/project/06_Project.cpp
+++ 수업준비/cplusplus/project/06_Project.cpp
@@ -6,95 +6,100 @@ using std::endl;
 using std::string;
 
 class Operation {
-    protected:
-        double num1, num2;
-        static double result;
-    
-    public:
-        Operation(double num1, double num2) {
-            this->num1 = num1;
-            this->num2 = num2;
-        }
+protected:
+    double num1, num2;
+    static double result;
 
-        virtual void calculate() = 0;
+public:
+    Operation(double num1, double num2) {
+        this->num1 = num1;
+        this->num2 = num2;
+    }
 
-        static double getResult() {
-            return result;
-        }
+    virtual void calculate() = 0;
+
+    static double getResult() {
+        return result;
+    }
 };
 
 double Operation::result = 0;
 
 class Add : public Operation {
-    public:
-        Add(double num1, double num2) : Operation(num1, num2) {};
-        void calculate() {
-            result = num1 + num2;
-        }
+public:
+    Add(double num1, double num2) : Operation(num1, num2) {};
+    void calculate() {
+        result = num1 + num2;
+    }
 };
 
 class Minus : public Operation {
-    public:
-        Minus(double num1, double num2) : Operation(num1, num2) {};
-        void calculate() {
-            result = num1 - num2;
-        }
+public:
+    Minus(double num1, double num2) : Operation(num1, num2) {};
+    void calculate() {
+        result = num1 - num2;
+    }
 };
 
 class Multiply : public Operation {
-    public:
-        Multiply(double num1, double num2) : Operation(num1, num2) {};
-        void calculate() {
-            result = num1 * num2;
-        }
+public:
+    Multiply(double num1, double num2) : Operation(num1, num2) {};
+    void calculate() {
+        result = num1 * num2;
+    }
 };
 
 class Divide : public Operation {
-    public:
-        Divide(double num1, double num2) : Operation(num1, num2) {};
-        void calculate() {
-            result = num1 / num2;
-        }
+public:
+    Divide(double num1, double num2) : Operation(num1, num2) {};
+    void calculate() {
+        result = num1 / num2;
+    }
 };
+
+double input_double() {
+    double num;
+    cout << "숫자를 입력해주세요 : ";
+    cin >> num;
+    
+    return num;
+}
+
 
 int main()
 {
     char op;
-    double number[2];
+    double number[2] = {};
     double num_in;
     Operation* operation;
-    char isContinue;
-    
+    string isContinue = "AC";
+
     while (1) {
 
-        if (number[0] != Operation::getResult()) {
-            cout << "숫자를 입력해주세요 : ";
-            cin >> num_in;
-            number[0] = num_in;
+        if (isContinue == "EXIT") break;
+        else if (isContinue == "AC") {
+            number[0] = input_double();
         }
 
         cout << "연산자를 입력해주세요 : ";
         cin >> op;
 
-        cout << "숫자를 입력해주세요 : ";
-        cin >> num_in;
-        number[1] = num_in;
-
+        number[1] = input_double();
 
         switch (op) {
-            case '+':
-                operation = new Add(number[0], number[1]);
-                break;
-            case '-':
-                operation = new Minus(number[0], number[1]);
-                break;
-            case '*':
-                operation = new Multiply(number[0], number[1]);
-                break;
-            default:
-                operation = new Divide (number[0], number[1]);
+        case '+':
+            operation = new Add(number[0], number[1]);
+            break;
+        case '-':
+            operation = new Minus(number[0], number[1]);
+            break;
+        case '*':
+            operation = new Multiply(number[0], number[1]);
+            break;
+        default:
+            operation = new Divide(number[0], number[1]);
 
-                break;
+            break;
         }
 
         operation->calculate();
@@ -103,9 +108,9 @@ int main()
         cout << "결과 : " << Operation::getResult() << endl;
         cout << "------------------------------------------------------\n";
 
-        cout << "연산을 계속 진행하시겠습니까? (y -> yes, n -> no) ";
+        cout << "연산을 계속 진행하시겠습니까? (Y: 계속, AC: 초기화, EXIT: 종료) ";
         cin >> isContinue;
-
-        if (isContinue == 'n') return 0;
     }
+
+    return 0;
 }
